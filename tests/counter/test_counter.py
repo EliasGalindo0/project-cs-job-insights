496211a8ae553793344597d399beeb9996ccb258
EliasGalindo0 <eliasgalindo@gmail.com>
2022-10-24 22:40:58 -0300

    feat: test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result_l = count_ocurrences('src/jobs.csv', 'test')
+    result_u = count_ocurrences('src/jobs.csv', 'TEST')
+    assert result_l == 2687
+    assert result_u == 2687
